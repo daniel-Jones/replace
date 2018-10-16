ac7d855e4e8202d99867cf6be3101bba5dd13001
daniel-Jones <daniel@danieljon.es>
2018-10-16 21:30:18 +1030

    added function to replace strings in memory as well, added an example to main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "replace.h"
 
 int
@@ -8,5 +9,8 @@ main(void)
 	ret = replaceinfile("input.txt", "output.txt", "%PLACEHOLDER%", "replacement string");
 	if (ret == 0)
 		puts("failed");
+	char *rep = replaceinmemory("this string this string is a test string", "string", "word");
+	printf("%s\n", rep);
+	free(rep);
 	return 0;
 }
